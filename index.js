3cb842d31df058bbebb7e29f73a99c22115bc394
Micah Johnson (work dktop) <mjohnson@teleflora.com>
2019-03-29 14:25:04 -0500

    changes to the images with mobile testing included

--- index.js
+++ index.js
@@ -8,32 +8,5 @@ $('.slick').slick({
           fade: true,
           speed: 2000,
           autoplay: true,
-          arrows: false,
-  
-    // the magic
-    responsive: [{
-  
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 1,
-          infinite: true,
-          fade: true,
-          speed: 2000,
-          autoplay: true
-        }
-  
-      }, {
-  
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          dots: true
-        }
-  
-      }, {
-  
-        breakpoint: 300,
-        settings: "unslick" // destroys slick
-  
-      }]
+          arrows: false
   });
